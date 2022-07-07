a9092e7f0af67c58d0eb8dae47fb5f34d68844da
Jason Wang <87679705+jasonthehung@users.noreply.github.com>
2022-07-07 23:29:47 +0800

    Add files via upload

---
+++ chapter2/src/HelloJava.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Hello, Java!");
+        frame.add(new HelloComponent());
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+}
